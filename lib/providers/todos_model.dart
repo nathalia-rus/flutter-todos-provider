6328c17b57661fa40d6299ad10c6e1fa2f83345d
Nathalia Rus <nathaliarus@live.fr>
2019-11-17 16:23:31 +0000

    todos model main simple get actions in provider

--- lib/providers/todos_model.dart
+++ lib/providers/todos_model.dart
@@ -3,3 +3,15 @@ import "dart:collection";
 import "package:flutter/material.dart";
 
 import "package:todos/models/task.dart";
+
+class TodoModel extends ChangeNotifier {
+  final List<Task> _tasks = [];
+
+  UnmodifiableListView<Task> get allTasks => UnmodifiableListView(_tasks);
+
+  UnmodifiableListView<Task> get incompleteTasks =>
+      UnmodifiableListView(_tasks.where((todo) => !todo.completed));
+
+  UnmodifiableListView<Task> get completedTasks =>
+      UnmodifiableListView(_tasks.where((todo) => todo.completed));
+}
