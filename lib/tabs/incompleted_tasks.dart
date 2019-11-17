8e7316e0f4b8f9ff0610ad2511e5d1de3633c6d1
Nathalia Rus <nathaliarus@live.fr>
2019-11-17 17:35:14 +0000

    incomplete tasks tab

---
+++ lib/tabs/incompleted_tasks.dart
@@ -0,0 +1,16 @@
+import 'package:todos/widgets/task_list.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todos/providers/todos_model.dart';
+
+class IncompleteTasks extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Consumer<TodosModel>(
+        builder: (context, todos, child) =>
+            TaskList(tasks: todos.incompleteTasks),
+      ),
+    );
+  }
+}
